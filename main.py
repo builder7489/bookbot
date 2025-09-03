9c24fe4e79a5d9d1e6eba94e8670d93262dfd7b6
builder7489 <larrydeontucker@protonmail.com>
2025-09-02 20:49:35 -0400

    added counting characters feature

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from stats import count_words
+from stats import count_chars
 
 def main():
     book = get_book_text("books/frankenstein_alt.txt")
@@ -6,6 +7,11 @@ def main():
     word_count = count_words(book)
     print(f"{word_count} words found in the document")
 
+    # testing counting characters feature
+    chars_count = count_chars(book)
+
+    for char in chars_count:
+        print(f"{char}:{chars_count[char]}")
 
 def get_book_text(filepath):
     with open(filepath) as file:
